e30638dab2018bb909ba3689c52179155539972a
Dokotela <grey.faulkenberry@mayjuun.com>
2024-03-10 00:06:18 -0500

    Added all the r6 resources from ndjson

--- new_fhir/ndjson_to_tests.dart
+++ new_fhir/ndjson_to_tests.dart
@@ -3,4 +3,14 @@ import 'dart:io';
 Future<void> main() async {
   final dir = Directory('./examples-ndjson');
   final files = await dir.list().toList();
+  for (var file in files) {
+    final contents = await File(file.path).readAsString();
+    final lines = contents.split('\n');
+    final type = file.toString().split('/').last.split('.').first;
+    for (int i = 0; i < lines.length; i++) {
+      if (lines[i].isNotEmpty) {
+        File('test/r6_examples/$type$i.json').writeAsStringSync(lines[i]);
+      }
+    }
+  }
 }
